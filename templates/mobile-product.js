cb9a492e295dd7a3be2353eed5b71135fbb4be68
antonioflore <floreantonio@hotmail.it>
2022-10-22 11:57:33 +0200

    fix: 3/3 pdp mobile

--- templates/mobile-product.js
+++ templates/mobile-product.js
@@ -101,7 +101,7 @@ const MobileProductTemplate = props => {
                   className="customStyle mb-10"
                   style={{
                     height: "100vh",
-                    overflow: "scroll",
+                    overflow: isExpanded ? "scroll" : "hidden",
                   }}
                 >
                   <div
