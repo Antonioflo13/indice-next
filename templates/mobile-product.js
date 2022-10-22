6a2510ddd88cad6a25be1543c958dd51dfb037aa
antonioflore <floreantonio@hotmail.it>
2022-10-22 12:44:02 +0200

    fix: 3/3 pdp mobile

--- templates/mobile-product.js
+++ templates/mobile-product.js
@@ -97,7 +97,7 @@ const MobileProductTemplate = props => {
             >
               <BottomSheet
                 defaultMode="collapsed"
-                height={heightPage - 300}
+                height={heightPage - 200}
                 style={{ pointerEvents: "all" }}
                 isExpanded={expanded => setIsExpanded(expanded)}
               >
