f42005118180bbbfdba7e6361dad586fb645166a
antonioflore <floreantonio@hotmail.it>
2022-10-24 23:22:19 +0200

    fix: 3/3 pdp mobile

--- templates/bottom-sheet.tsx
+++ templates/bottom-sheet.tsx
@@ -19,9 +19,9 @@ interface Props {
 
 const getY = (height: number, mode: Mode): number => {
   if (mode === "collapsed") {
-    return height * 0.93;
+    return height * 0.95;
   }
-  return height * 0.13;
+  return height * 0.2;
 };
 
 const BottomSheet: React.FC<Props> = ({
