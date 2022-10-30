094ec392b5458f0f37af60c254bbc181f2bdc7bd
antonioflore <floreantonio@hotmail.it>
2022-10-30 17:05:50 +0100

    merge: add store

--- pages/magazine/[article].js
+++ pages/magazine/[article].js
@@ -32,103 +32,111 @@ const Article = ({ article, collectionProducts }) => {
   // });
 
   return (
-    <Layout>
-      <AnimatedPage margins={true}>
-        <div className="flex flex-col justify-center w-full">
-          <div className="w-full md:w-1/2 mt-8">
-            {isDesktop && (
-              <PageTitle
-                breadcrumbs={[
-                  ...[
-                    { title: "breadcrumbs.magazine", link: "/magazine" },
-                    {
-                      title: article?.handle,
-                    },
-                  ],
-                ]}
-                title=" "
-                subtitle=" "
-              />
-            )}
-          </div>
-          {/* Info */}
-          <div className="flex container-collection mt-4">
-            {isDesktop ? (
-              <img
-                className="img-headerCollection"
-                src={article.imageheader.url}
-                alt={article.imageheader.url}
-              />
-            ) : (
-              <img
-                className="img-headerCollection"
-                src={article.imageheadermobile.url}
-                alt={article.imageheader.url}
-              />
-            )}
+    <>
+      {article && (
+        <Layout>
+          <AnimatedPage margins={true}>
+            <div className="flex flex-col justify-center w-full">
+              <div className="w-full md:w-1/2 mt-8">
+                {isDesktop && (
+                  <PageTitle
+                    breadcrumbs={[
+                      ...[
+                        { title: "breadcrumbs.magazine", link: "/magazine" },
+                        {
+                          title: article?.handle,
+                        },
+                      ],
+                    ]}
+                    title=" "
+                    subtitle=" "
+                  />
+                )}
+              </div>
+              {/* Info */}
+              <div className="flex container-collection mt-4">
+                {isDesktop ? (
+                  <img
+                    className="img-headerCollection"
+                    src={article.imageheader.url}
+                    alt={article.imageheader.url}
+                  />
+                ) : (
+                  <img
+                    className="img-headerCollection"
+                    src={article.imageheadermobile.url}
+                    alt={article.imageheader.url}
+                  />
+                )}
 
-            <div>
-              <h1 className="uppercase font-bold my-10 text-center">
-                {article.title}
-              </h1>
+                <div>
+                  <h1 className="uppercase font-bold my-10 text-center">
+                    {article.title}
+                  </h1>
+                </div>
+                <div className="text-center">
+                  <div>
+                    <img
+                      className="float-left mr-5 mb-5"
+                      src={article.image.url}
+                      alt={""}
+                      width={"200vw"}
+                    />
+                  </div>
+                  <p className="text-justify">
+                    {article.description.length > 0
+                      ? article.description
+                      : null}
+                  </p>
+                </div>
+              </div>
             </div>
-            <div className="text-center">
-              <div>
-                <img
-                  className="float-left mr-5 mb-5"
-                  src={article.image.url}
-                  alt={""}
-                  width={"200vw"}
+            {collectionProducts !== null && (
+              <SliderArticleCollection
+                collectionProducts={collectionProducts}
+              />
+            )}
+            {/*<SliderArticleProducts productsinArticle={productsinArticle} />*/}
+            {!isDesktop && (
+              <div className="mt-10">
+                <PageTitle
+                  breadcrumbs={[
+                    ...[
+                      { title: "breadcrumbs.magazine", link: "/magazine" },
+                      {
+                        title: article.handle,
+                      },
+                    ],
+                  ]}
+                  title=" "
+                  subtitle=" "
                 />
               </div>
-              <p className="text-justify">
-                {article.description.length > 0 ? article.description : null}
-              </p>
-            </div>
-          </div>
-        </div>
-        {/*{collectionProducts !== null && (*/}
-        {/*  <SliderArticleCollection collectionProducts={collectionProducts} />*/}
-        {/*)}*/}
-        {/*<SliderArticleProducts productsinArticle={productsinArticle} />*/}
-        {!isDesktop && (
-          <div className="mt-10">
-            <PageTitle
-              breadcrumbs={[
-                ...[
-                  { title: "breadcrumbs.magazine", link: "/magazine" },
-                  {
-                    title: article.handle,
-                  },
-                ],
-              ]}
-              title=" "
-              subtitle=" "
-            />
-          </div>
-        )}
-      </AnimatedPage>
-      <style jsx="true">
-        {`
-          .container-collection {
-            flex-direction: column;
-          }
+            )}
+          </AnimatedPage>
+          <style jsx="true">
+            {`
+              .container-collection {
+                flex-direction: column;
+              }
 
-          .img-headerCollection {
-            width: 100%;
-            height: 30vh;
-            border-radius: 15px;
-            object-fit: cover;
-          }
+              .img-headerCollection {
+                width: 100%;
+                height: 30vh;
+                border-radius: 15px;
+                object-fit: cover;
+              }
 
-          @media (max-width: 768px) {
-            .img-headerCollection {
-              height: 200px;
-            }
-          }
-        `}
-      </style>
-    </Layout>
+              @media (max-width: 768px) {
+                .img-headerCollection {
+                  height: 200px;
+                }
+              }
+            `}
+          </style>
+        </Layout>
+      )}
+    </>
   );
 };
 
